5e22c2ff1f26de61ca7651602a8f7e54a11a2ee4
Priya.Jain <Priya.Jain@IB-SEZ-PC-490.infobeans.in>
2019-06-03 10:09:08 +0530

    changes class and variable name

---
+++ ExcelReadtest/src/main/java/assignment3automatiom/ExcelReadtest/Lang.java
@@ -0,0 +1,13 @@
+package assignment3automatiom.ExcelReadtest;
+
+public class Lang {
+	public String first;
+	public String second;
+
+
+	public Lang(String language1, String language2) {
+		this.first = language1;
+		this.second = language2;
+
+	}
+}
